e84cfc91feb62dc7f73e419b90b62676f55655e3
Chris Jenkins <chris.jenkins@blackbaud.com>
2017-06-06 17:01:39 -0500

    sync files based on md5 instead of blindly copying

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"io/ioutil"
 	"github.com/azure/azure-sdk-for-go/storage"
 	"path/filepath"
+	"crypto/md5"
+	"io"
+	"encoding/hex"
+	"encoding/base64"
 )
 
 
@@ -13,6 +17,7 @@ var (
 	accountName	string
 	accountKey	string
 	client		storage.BlobStorageClient
+	container	*storage.Container
 )
 
 func init() {
@@ -26,10 +31,13 @@ func init() {
 
 func main() {
 	dirs := os.Args[1:]
-	filepaths := getFiles(dirs)
 
+	container = createContainer("humane")
+
+	filepaths := getFiles(dirs)
+	existingBlobs := getExistingBlobs()
 	if len(filepaths) > 0 {
-		syncFiles(filepaths)
+		syncFiles(filepaths, existingBlobs)
 	}
 }
 
@@ -38,26 +46,74 @@ func getFiles(dirs []string) []string {
 	for _, aDir := range dirs {
 		fileInfo, _ := ioutil.ReadDir(aDir)
 		for _, file := range fileInfo {
-			paths = append(paths, filepath.Join(aDir, file.Name()))
+			path := filepath.Join(aDir, file.Name())
+			paths = append(paths, path)
 		}
 	}
 	return paths
 }
 
-func syncFiles(files []string) {
-	container := createContainer("humane")
+func getExistingBlobs() map[string]string {
+	r, err := container.ListBlobs(storage.ListBlobsParameters{})
+	onErrorFail(err, "Could not list existing blobs")
+	blobs := make(map[string]string)
 
+	for _, blob := range r.Blobs {
+		md5bytes, _ := base64.StdEncoding.DecodeString(blob.Properties.ContentMD5)
+		md5str := hex.EncodeToString(md5bytes)
+		blobs[blob.Name] = md5str
+	}
+
+	return blobs
+}
+
+func getFileMd5(filePath string) (string, error) {
+	var returnMd5String string
+	file, err := os.Open(filePath)
+	if err != nil {
+		return returnMd5String, err
+	}
+	defer file.Close()
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return returnMd5String, err
+	}
+	hashInBytes := hash.Sum(nil)[:16]
+	returnMd5String = hex.EncodeToString(hashInBytes)
+	return returnMd5String, nil
+
+}
+
+
+func syncFiles(files []string, blobs map[string]string) {
 	for _, file := range files {
-		fmt.Printf("Creating blob for %s\n", file)
+
 		baseFilename := filepath.Base(file)
+
+		if blobExists(file, blobs[baseFilename]) {
+			continue
+		}
+
+		fmt.Printf("Creating blob for %s\n", file)
 		blobOptions := &storage.PutBlobOptions{}
 		blob := container.GetBlobReference(baseFilename)
 
 		filehandle := openFileOrFail(file)
+		defer filehandle.Close()
 
 		err := blob.CreateBlockBlobFromReader(filehandle, blobOptions)
 		onErrorFail(err, fmt.Sprintf("Failed to upload %s", file))
 	}
+}
+
+func blobExists(file string, blobMd5 string) bool {
+	if blobMd5 != "" {
+		fileMd5, _ := getFileMd5(file)
+		if blobMd5 == fileMd5 {
+			return true
+		}
+	}
+	return false
 }
 
 func openFileOrFail(f string) *os.File {
